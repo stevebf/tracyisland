a774da3600741e3538a1a5c74892cfcf9a86a2f0
stevebf <stevemainprize@gmail.com>
2015-10-14 17:51:46 +0100

    Added SMS capability

--- check.py
+++ check.py
@@ -16,6 +16,13 @@ time.sleep(10)
 
 GPIO.setmode(GPIO.BOARD)
 
+# Test IFTTT
+# url = "https://maker.ifttt.com/trigger/connected_to_wifi/with/key/ceOSB-sJQgQqYMVKkQeeGa?value1=Reboot"
+# try:
+#     response = urllib2.urlopen(url)
+# except:
+#     response = ""
+
 try:
     # create the lists of devices, owners and pins
     # Searching the HTML from the router will use a convention of putting
@@ -24,6 +31,7 @@ try:
     devicelist=["android-74e7b30847903cbd","stevemaesiphone","sarahs-ipad","davys-iphone"]
     pinlist=[15,16,18,22]
     ownerlist=["Richie","Steve","Sarah","Davy"]
+    statuslist=["unknown","unknown","unknown","unknown"]
 
     while True:
         # this is a file to which we'll write the output for debugging
@@ -58,11 +66,42 @@ try:
                     # f.write(ownerlist[i] + " is here"+"\n")
                     # print(ownerlist[i] + " is here")
                     GPIO.output(pinlist[i],True)
+
+                    if statuslist[i] == "out":
+                        # Use IFTTT to send a text to say this person is now home
+                        url = "https://maker.ifttt.com/trigger/connected_to_wifi/with/key/ceOSB-sJQgQqYMVKkQeeGa?value1="+ownerlist[i]
+                        try:
+                            response = urllib2.urlopen(url)
+                        except:
+                            response = ""
+
+                        if response == "":
+                            # No good response from IFTT
+                            print(now)
+                            print("IFTTT didn't respond")
+
+                    statuslist[i] = "in"
+
                 else:
                     # if the device is not found, turn on the corresponding LED
                     # f.write(ownerlist[i] + " is not here"+"\n")
                     # print(ownerlist[i] + " is not here")
                     GPIO.output(pinlist[i],False)
+
+                    if statuslist[i] == "in":
+                        # Use IFTTT to send a text to say this person is now out
+                        url = "https://maker.ifttt.com/trigger/disconnected_from_wifi/with/key/ceOSB-sJQgQqYMVKkQeeGa?value1="+ownerlist[i]
+                        try:
+                            response = urllib2.urlopen(url)
+                        except:
+                            response = ""
+
+                        if response == "":
+                            # No good response from IFTT
+                            print(now)
+                            print("IFTTT didn't respond")
+
+                    statuslist[i] = "out"
 
             # f.close
 
